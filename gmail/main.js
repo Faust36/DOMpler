69e63481dcd8de3e77250994831a9e2abee6a624
faust36 <jonathanxfish@gmail.com>
2018-03-20 11:48:51 -0400

    Refactor some methods

--- gmail/main.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", (e) => {
-  $('.sidebar-nav li').each ((idx, el) => {
-    el.addEventListener("click", (event)=> {
-      const newLoc = event.target.innerText.toLowerCase();
-      window.location.hash = newLoc;
-    });
-  });
-});
